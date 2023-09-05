fb7f50f7e92fa776c3011cb7969ebead7db9db49
Andrei Tudorache <andreit@stonesouptech.com>
2023-09-05 19:07:59 +0300

    data-structures necessary for find-email functionality

---
+++ include/ZeroBounce/ZBFindEmailResponse.h
@@ -0,0 +1,147 @@
+#ifndef ZB_FIND_EMAIL_RESPONSE_H
+#define ZB_FIND_EMAIL_RESPONSE_H
+
+#include <json.h>
+
+/**
+ * @brief highlights how a domain can format its emails and how confident is
+ * the API about it
+ */
+typedef struct {
+    char* format;
+    char* confidence;
+} ZBDomainFormat;
+
+/**
+ * @brief array of data format objects
+ *
+ */
+typedef struct {
+    ZBDomainFormat* data;
+    size_t size;
+} ZBDomainFormatVector;
+
+/**
+ * @brief The struct associated with the GET /guessformat request.
+ *
+ */
+typedef struct {
+    char* email;
+    char* domain;
+    char* format;
+    char* status;
+    char* sub_status;
+    char* confidence;
+    char* did_you_mean;
+    char* failure_reason;
+    ZBDomainFormatVector other_domain_formats;
+} ZBFindEmailResponse;
+
+
+// ZBDomainFormat methods
+
+/**
+ * @brief Function used to initialize a new ZBDomainFormat.
+ *
+ * @return ZBDomainFormat new instance
+ */
+ZBDomainFormat zb_domain_format_init();
+
+/**
+ * @brief Function used to serialize a ZBDomainFormat.
+ *
+ * @param response ZBDomainFormat pointer
+ * @return char* serialization
+ */
+char* zb_domain_format_to_string(ZBDomainFormat* domain_format);
+
+/**
+ * @brief Function used to create ZBDomainFormat from a json object.
+ *
+ * @param j json pointer
+ * @return ZBDomainFormat new instance
+ */
+ZBDomainFormat zb_domain_format_from_json(const json_object* j);
+
+/**
+ * @brief Function used to free the memory of a ZBDomainFormat instance.
+ *
+ * @param vector ZBDomainFormat pointer
+ */
+void zb_domain_format_free(ZBDomainFormat* instance);
+
+
+// ZBDomainFormatVector methods
+
+/**
+ * @brief Function used to initialize a new ZBDomainFormatVector.
+ *
+ * @return ZBDomainFormatVector new instance
+ */
+ZBDomainFormatVector zb_domain_format_vector_init();
+
+/**
+ * @brief Function used to add a ZBDomainFormat instance to the vector.
+ *
+ * @param vector ZBDomainFormatVector vector pointer
+ * @param instance ZBDomainFormat to be added
+ */
+void zb_domain_format_vector_append(ZBDomainFormatVector* vector, const ZBDomainFormat instance);
+
+/**
+ * @brief Function used to free the memory of a ZBDomainFormatVector instance.
+ *
+ * @param vector ZBDomainFormatVector pointer
+ */
+void zb_domain_format_vector_free(ZBDomainFormatVector* vector);
+
+/**
+ * @brief Function used to serialize a ZBDomainFormatVector.
+ *
+ * @param response ZBDomainFormatVector pointer
+ * @return char* serialization
+ */
+char* zb_domain_format_vector_to_string(ZBDomainFormatVector* vector);
+
+/**
+ * @brief Function used to create ZBDomainFormatVector from a json object.
+ *
+ * @param j json pointer
+ * @return ZBDomainFormatVector new instance
+ */
+ZBDomainFormatVector zb_domain_format_vector_from_json(const json_object* j);
+
+
+// ZBFindEmailResponse methods
+
+/**
+ * @brief Function used to initialize a new ZBFindEmailResponse.
+ *
+ * @return ZBFindEmailResponse new instance
+ */
+ZBFindEmailResponse zb_find_email_response_init();
+
+/**
+ * @brief Function used to serialize a ZBFindEmailResponse.
+ *
+ * @param response ZBFindEmailResponse pointer
+ * @return char* serialization
+ */
+char* zb_find_email_response_to_string(ZBFindEmailResponse* response);
+
+/**
+ * @brief Function used to create ZBFindEmailResponse from a json object.
+ *
+ * @param j json pointer
+ * @return ZBFindEmailResponse new instance
+ */
+ZBFindEmailResponse zb_find_email_response_from_json(const json_object* j);
+
+/**
+ * @brief Function used to free the memory of a ZBFindEmailResponse instance.
+ *
+ * @param vector ZBFindEmailResponse pointer
+ */
+void zb_find_email_response_free(ZBFindEmailResponse* instance);
+
+#endif
---
+++ src/ZBFindEmailResponse.c
@@ -0,0 +1,267 @@
+#include <stdio.h>
+
+#include "ZeroBounce/ZBFindEmailResponse.h"
+#include "ZeroBounce/utils.h"
+
+// ZBDomainFormat methods
+
+ZBDomainFormat zb_domain_format_init() {
+    ZBDomainFormat domain_format;
+    domain_format.format = NULL;
+    domain_format.confidence = NULL;
+    return domain_format;
+}
+
+char* zb_domain_format_to_string(ZBDomainFormat* domain_format) {
+    const char* serialization = "ZBDomainFormat{format='%s',confidence='%s'}";
+    int size = snprintf(
+        NULL, 0, serialization,
+        domain_format->format,
+        domain_format->confidence
+    );
+    if (size < 0) {
+        return NULL;
+    }
+
+    char* buffer = (char*) malloc(size + 1);
+    if (!buffer) {
+        return NULL;
+    }
+
+    snprintf(
+        buffer,
+        size + 1,
+        serialization,
+        domain_format->format,
+        domain_format->confidence
+    );
+    return buffer;
+}
+
+
+ZBDomainFormat zb_domain_format_from_json(const json_object* j) {
+    ZBDomainFormat domain_format = zb_domain_format_init();
+    domain_format.format = *(char**)get_json_value(j, json_type_string, "format", &(char*){""});
+    domain_format.confidence = *(char**)get_json_value(j, json_type_string, "confidence", &(char*){""});
+    return domain_format;
+}
+
+void zb_domain_format_free(ZBDomainFormat* instance) {
+    if (instance == NULL) return;
+
+    free(instance->format);
+    free(instance->confidence);
+    instance->format = NULL;
+    instance->confidence = NULL;
+}
+
+
+// ZBDomainFormatVector methods
+
+ZBDomainFormatVector zb_domain_format_vector_init() {
+    ZBDomainFormatVector vector;
+    vector.size = 0;
+    vector.data = NULL;
+    return vector;
+}
+
+void zb_domain_format_vector_append(ZBDomainFormatVector* vector, const ZBDomainFormat instance) {
+    if (!vector) return;
+
+    vector->data = (ZBDomainFormatVector*) realloc(vector->data, (vector->size + 1) * sizeof(ZBDomainFormatVector));
+    if (!vector->data) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    vector->data[vector->size].format = strdup(instance.format);
+    vector->data[vector->size].confidence = strdup(instance.confidence);
+    vector->size++;
+}
+
+void zb_domain_format_vector_free(ZBDomainFormatVector* vector) {
+    for (int i = 0; i <= vector->size; i++) {
+        zb_domain_format_free(vector->data+i);
+    }
+    free(vector->data);
+    vector->data = NULL;
+    vector->size = 0;
+}
+
+char* zb_domain_format_vector_to_string(ZBDomainFormatVector* vector){
+    char* final_string;
+
+    if (vector->size == 0) {
+        return "[]";
+    }
+
+    StringVector string_vector = string_vector_init();
+    if (vector->size == 1) {
+        char* item_serialization = zb_domain_format_to_string(vector->data + 0);
+        if (item_serialization == NULL) {
+            return NULL;
+        }
+        string_vector_append(&string_vector, "[");
+        string_vector_append(&string_vector, item_serialization);
+        string_vector_append(&string_vector, "]");
+
+        final_string = concatenate_strings(&string_vector);
+        string_vector_free(&final_string);
+        free(item_serialization);
+        return final_string;
+    }
+
+    char* item_serialization; // used to free individual serializations
+    item_serialization = zb_domain_format_to_string(vector->data + 0);
+
+    string_vector_append(&string_vector, "[");
+    string_vector_append(&string_vector, item_serialization);
+    free(item_serialization);
+    for (int i = 0; i < vector->size; i++) {
+        item_serialization = zb_domain_format_to_string(vector->data + i);
+        string_vector_append(&string_vector, ",");
+        string_vector_append(&string_vector, item_serialization);
+        free(item_serialization);
+    }
+    string_vector_append(&string_vector, "]");
+
+    final_string = concatenate_strings(&string_vector);
+    string_vector_free(&final_string);
+    return final_string;
+}
+
+ZBDomainFormatVector zb_domain_format_vector_from_json(const json_object* j) {
+    if (json_object_get_type(j) != json_type_array) {
+        fprintf(
+            stderr,
+            "Invalid json type found for domain format vector.\n"
+            "Expected 'json_type_array', found '%s'.\n",
+            json_type_to_name(json_object_get_type(j))
+        );
+        exit(EXIT_FAILURE);
+    }
+
+    size_t vector_size = json_object_array_length(j);
+    ZBDomainFormatVector vector = zb_domain_format_vector_init();
+    vector.data = malloc(vector_size * sizeof(ZBDomainFormat));
+    vector.size = vector_size;
+    for (int index = 0; index < vector_size; index ++) {
+        vector.data[index] = zb_domain_format_from_json(
+            json_object_array_get_idx(j, index)
+        );
+    }
+    return vector;
+}
+
+// ZBFindEmailResponse methods
+
+ZBFindEmailResponse zb_find_email_response_init() {
+    ZBFindEmailResponse response;
+    response.email = NULL;
+    response.domain = NULL;
+    response.format = NULL;
+    response.status = NULL;
+    response.sub_status = NULL;
+    response.confidence = NULL;
+    response.did_you_mean = NULL;
+    response.failure_reason = NULL;
+    response.other_domain_formats = zb_domain_format_vector_init();
+    return response;
+}
+
+char* zb_find_email_response_to_string(ZBFindEmailResponse* response) {
+    char* domain_serialization;
+    const char* serialization = "ZBFindEmailResponse{"
+        "email=\"%s\", "
+        "domain=\"%s\", "
+        "format=\"%s\", "
+        "status=\"%s\", "
+        "sub_status=\"%s\", "
+        "confidence=\"%s\", "
+        "did_you_mean=\"%s\", "
+        "failure_reason=\"%s\", "
+        "other_domain_formats=%s}";
+
+    domain_serialization = zb_domain_format_vector_to_string(
+        &(response->other_domain_formats)
+    );
+    int size = snprintf(
+        NULL,
+        0,
+        serialization,
+        response->email,
+        response->domain,
+        response->format,
+        response->status,
+        response->sub_status,
+        response->confidence,
+        response->did_you_mean,
+        response->failure_reason,
+        domain_serialization
+    );
+    if (size < 0) {
+        return NULL;
+    }
+
+    char* buffer = (char*) malloc(size + 1);
+    if (!buffer) {
+        return NULL;
+    }
+
+    snprintf(
+        buffer,
+        size + 1,
+        serialization,
+        response->email,
+        response->domain,
+        response->format,
+        response->status,
+        response->sub_status,
+        response->confidence,
+        response->did_you_mean,
+        response->failure_reason,
+        domain_serialization
+    );
+    free(domain_serialization);
+    return buffer;
+}
+
+ZBFindEmailResponse zb_find_email_response_from_json(const json_object* j) {
+    ZBFindEmailResponse response = zb_find_email_response_init();
+    response.email = *(char**)get_json_value(j, json_type_string, "email", &(char*){""});
+    response.domain = *(char**)get_json_value(j, json_type_string, "domain", &(char*){""});
+    response.format = *(char**)get_json_value(j, json_type_string, "format", &(char*){""});
+    response.status = *(char**)get_json_value(j, json_type_string, "status", &(char*){""});
+    response.sub_status = *(char**)get_json_value(j, json_type_string, "sub_status", &(char*){""});
+    response.confidence = *(char**)get_json_value(j, json_type_string, "confidence", &(char*){""});
+    response.did_you_mean = *(char**)get_json_value(j, json_type_string, "did_you_mean", &(char*){""});
+    response.failure_reason = *(char**)get_json_value(j, json_type_string, "failure_reason", &(char*){""});
+
+    json_object* empty_list = json_object_new_array_ext(0);
+    response.other_domain_formats = zb_domain_format_vector_from_json(
+       get_json_value(j, json_type_array, "other_domain_formats", empty_list)
+    );
+    json_object_array_entry_free(empty_list);
+
+    return response;
+}
+
+void zb_find_email_response_free(ZBFindEmailResponse* instance) {
+    zb_domain_format_vector_free(&(instance->other_domain_formats));
+    free(instance->email);
+    free(instance->domain);
+    free(instance->format);
+    free(instance->status);
+    free(instance->sub_status);
+    free(instance->confidence);
+    free(instance->did_you_mean);
+    free(instance->failure_reason);
+    instance->email = NULL;
+    instance->domain = NULL;
+    instance->format = NULL;
+    instance->status = NULL;
+    instance->sub_status = NULL;
+    instance->confidence = NULL;
+    instance->did_you_mean = NULL;
+    instance->failure_reason = NULL;
+}
